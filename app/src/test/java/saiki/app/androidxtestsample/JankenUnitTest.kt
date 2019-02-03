f284adb1dc4ebc33b39badfab4be35061f6974fb
saiki <otoiawasehakotiradayo@gmail.com>
2019-02-03 14:20:31 +0900

    直前の状態

--- app/src/test/java/saiki/app/androidxtestsample/JankenUnitTest.kt
+++ app/src/test/java/saiki/app/androidxtestsample/JankenUnitTest.kt
@@ -1,33 +1,17 @@
 package saiki.app.androidxtestsample
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
-import saiki.app.androidxtestsample.Janken.Result.P1_WIN
-import saiki.app.androidxtestsample.Janken.Result.P2_WIN
-
 class JankenUnitTest {
-    private val subject = Janken()
 
-    @Test
-    fun janken_グー対チョキなら1pが勝つ() {
-        val result = subject.start("guu","choki")
+//    @Test
+//    fun janken_グー対チョキなら1pが勝つ() {
+//    }
 
-        assertEquals(P1_WIN,result)
-    }
+//    @Test
+//    fun janken_チョキ対パーなら1pが勝つ() {
+//    }
 
-    @Test
-    fun janken_チョキ対パーなら1pが勝つ() {
-        val result = subject.start("choki","paa")
-
-        assertEquals(P1_WIN,result)
-    }
-
-    @Test
-    fun janken_グー対パーなら2pが勝つ() {
-        val result = subject.start("guu","paa")
-
-        assertEquals(P2_WIN,result)
-    }
-
+//    @Test
+//    fun janken_グー対パーなら2pが勝つ() {
+//    }
 
 }
